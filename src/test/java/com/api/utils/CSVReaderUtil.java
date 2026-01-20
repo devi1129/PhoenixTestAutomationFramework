88b4c433457994185e375c5e452d1b15117fb489
devipothabat_mgy96j0 <devipothabat_mgy96j0@SPFHYLL-0292>
2026-01-20 15:30:06 +0530

    CSVReaderUtil: load the csv file and map it to UserBean
    UserBean: Bean class for CSV mapping and will be used by Dataproviders

---
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -0,0 +1,46 @@
+package com.api.utils;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.dataproviders.api.Bean.UserBean;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CSVReaderUtil {
+	
+	
+	private CSVReaderUtil()
+	{
+		
+	}
+	
+	
+	public static void loadCSV(String path){
+		
+		
+		
+		 InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+		 
+		
+		 InputStreamReader isr=new InputStreamReader(resourceAsStream);
+		
+		
+		CSVReader csv=new CSVReader(isr);
+		
+
+		
+		CsvToBean<UserBean> creds=new CsvToBeanBuilder(csv).withType(UserBean.class)
+				                   .withIgnoreEmptyLine(true)
+				                   .build();
+		
+		List<UserBean> list = creds.parse();
+		
+		System.out.println(list.toString());
+		System.out.println(list.get(0).getUsername());
+		
+	}
+
+}
